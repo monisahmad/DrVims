a2e41265cfd886339a8f13523d198cb86a796db4
monisahmad <monis.ahmad42@gmail.com>
2018-08-23 18:06:54 +0530

    feat: Randomized Number of Coins

--- src/App.js
+++ src/App.js
@@ -1,58 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const sleep = timeInMS => (
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeInMS);
+  })
+);
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 12,
+      count: Math.random() * 20,
       game: 1,
-      computer: 0,
+      winner: '',
+      player: 'Player 1',
     };
     this.increment = this.increment.bind(this);
     this.resetGame = this.resetGame.bind(this);
   }
 
   async increment(event) {
-    let count = this.state.count - event.target.value;
-    await sleep(500);
-    this.setState({ count, computer: 0 });
-    let comp = 0;
-    while (count % 4 !== 0) {
-      await sleep(500);
-      count -= 1;
-      comp += 1;
-      this.setState({ count, computer: comp });
-    }
+    const count = this.state.count - event.target.value;
     if (count <= 0) {
-      this.setState({ game: 0, count: 0 });
+      this.setState({ winner: this.state.player, game: 0 });
+    }
+    await sleep(500);
+    this.setState({ count });
+    if (this.state.player === 'Player 1') {
+      this.setState({ player: 'Player 2' });
+    } else {
+      this.setState({ player: 'Player 1' });
     }
   }
 
   resetGame() {
     this.setState({
-      count: 12,
+      count: Math.random() * 20,
       game: 1,
-      computer: 0,
+      winner: '',
+      player: 'Player 1',
     });
   }
 
 
   render() {
-    const { game, computer } = this.state;
-    let message = '';
-    if (computer === 0) {
-      message = 'Draw Some Coins';
-    } else {
-      message = `Computer has drawn ${computer} coins`;
-    }
+    const {
+      game, player, winner, count,
+    } = this.state;
+    const message = `${player}'s Turn`;
     const coins = [];
-    for (let i = 0; i < this.state.count; i += 1) {
+
+    for (let i = 0; i < count; i += 1) {
       const circleStyle = {
         width: 80,
         height: 80,
         border: '3px solid black',
-        float: 'left',
+        display: 'inline-block',
         margin: 10,
         borderRadius: '50%',
       };
@@ -68,7 +73,7 @@ class App extends Component {
     if (game) {
       return (
         <div className="App">
-          <div>
+          <div >
             {coins}
           </div>
           <h2 >{message}</h2>
@@ -83,22 +88,13 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>{this.state.count}</h1>
         <h1>Game Over</h1>
-        <h2>{message}</h2>
-        <h2> Computer Won </h2>
+        <h2> {winner} Wins!!! </h2>
         <button onClick={this.resetGame}>Restart</button>
       </div>
     );
   }
 }
 
-const sleep = timeInMS => (
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, timeInMS);
-  })
-);
 
 export default App;
